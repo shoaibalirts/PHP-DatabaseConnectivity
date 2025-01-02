b9b9f84d96dd0269ebb63f5ff555123907b1b23f
shoaibalirts <shoaibali.rts@gmail.com>
2025-01-02 21:22:38 +0100

    fetched record using select, inserted and deleted records using insert and delete sql statements respectively

--- index.php
+++ index.php
@@ -20,42 +20,67 @@
     </p>
 
     <?php
-    $id=2;
+    $id="";
     function e($value)
     {
         return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
     }
     if (!empty($_GET['id'])) {
-        $id = $_GET['id'];
+        $id = (int) $_GET['id'];
+    } else {
+        $id=1;
     }
-    var_dump($_GET['id']);
+    // var_dump($_GET['id']);
     ?>
+
     <?php
     $pdo = new PDO('mysql:host=localhost;dbname=note_app', 'root', '', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
-    // var_dump($pdo);
-    // $title = addslashes("The Basics I've Learned");
+   
 
-    $query = "SELECT * FROM `notes` WHERE `id`=$id ";
+    // $query = "SELECT * FROM `notes` WHERE `id`={$id} ";
 
     // create a statement to prepare a query
 
-    $stmt = $pdo->prepare($query);
+    $stmt = $pdo->prepare('SELECT * FROM `notes` WHERE `id`=:id ');// placed a placeholder as id
+    // INSERT ROW
+    /*
+    $title='A title (from PHP)';
+    $content = 'The content (from PHP)';
+    $stmt = $pdo->prepare("INSERT INTO `notes` (`title`,`content`) VALUES (:title, :content)");
+    $stmt->bindValue('title',$title);
+    $stmt->bindValue('content',$content);
+    */
+    
+    /* UPDATE ROW
+    $stmt=$pdo->prepare("UPDATE `notes` SET `title` = :title, `content` = :content WHERE `id` = :id");
+    $stmt->bindValue('id', 14);
+    $stmt->bindValue('title', 'TITLE (from Php)');
+    $stmt->bindValue('content','CONTENT (from Php)');
     //execute the statement
     $stmt->execute();
-    // fetch all results
+    */
+
+    // DELETE ROW
+    $stmt2 = $pdo->prepare("DELETE FROM notes WHERE `notes`.`id` = :id"); // placeholder for protecting sql injection because data can come form url or from variable
+    $stmt2->bindValue('id',11);
+    $stmt2->execute();
+    
+    /* FETCH A SINGLE ROWS 
     // $note = $stmt->fetch(PDO::FETCH_ASSOC);
-    $note = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    // $note = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    var_dump($note);
+    // var_dump($note);
+    */
     ?>
 
 
 </body>
 
 </html>
+
 <?php
 /*
 $zip = new ZipArchive();

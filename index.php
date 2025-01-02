962477b9da8f9ae8214bf6d964a7e753e4215a66
shoaibalirts <shoaibali.rts@gmail.com>
2025-01-02 13:06:38 +0100

    fetch vs fetchAll

--- index.php
+++ index.php
@@ -10,17 +10,34 @@
 
 <body>
     <?php
+    function e($value)
+    {
+        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
+    ?>
+    <?php
     $pdo = new PDO('mysql:host=localhost;dbname=note_app', 'root', '', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
     // var_dump($pdo);
-
+    $title = addslashes("The Basics I've Learned");
+    $query = "SELECT * FROM `notes` WHERE `title` = '$title' ";
+    $query = "SELECT * FROM `notes` WHERE `id`=2 OR `id`=6 ";
     // create a statement to prepare a query
     $stmt = $pdo->prepare('SELECT * FROM `notes` ');
+    $stmt = $pdo->prepare('SELECT * FROM `notes` ORDER BY `notes`.`title` DESC');
+    $stmt = $pdo->prepare('SELECT `id`, `title`,`content` FROM `notes` ORDER BY `notes`.`id` ASC');
+    $stmt = $pdo->prepare($query);
     //execute the statement
     $stmt->execute();
     // fetch all results
+    // $note = $stmt->fetch(PDO::FETCH_ASSOC);
+    while (($result = $stmt->fetch(PDO::FETCH_ASSOC)) !== false) {
+        // $results[];
+    }
+    // $results = $stmt->fetch(PDO::FETCH_ASSOC);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
     var_dump($results);
     ?>
 
@@ -28,8 +45,9 @@
 
         <?php foreach ($results as $result): ?>
             <div class="container">
-                <li><?php echo "Title: ".$result['title'] ?></li>
-                <li><?php echo "Content: ".$result['content'] ?></li>
+                <li><?php echo e($result['id']) ?></li>
+                <li><?php echo e($result['title']) ?></li>
+                <li><?php echo e($result['content']) ?></li>
             </div>
         <?php endforeach; ?>
     </ul>
